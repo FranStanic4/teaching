2885af90b9078dfb7626912de2bdc7bdbed9dd14
FranStanic4 <fran.stanic4@skole.hr>
2025-10-28 09:14:08 +0100

    Update 02-loops-2.py

--- 02-loops-2.py
+++ 02-loops-2.py
@@ -2,3 +2,15 @@
 #
 # there are two kinds of loops for loop and while loop
 
+# for loop is used when amount of iteration is known
+
+for n in range(2):
+    print(n)
+
+terminal:
+0
+1
+
+# while loop is used when we dont know the amount of iterations
+
+# for both loops you can use break to exit out of the loop early
